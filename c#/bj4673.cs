ef71a4d0bb779fde427d4c216c05c49f81b28ad0
Hosugi <108512016+Hosugi@users.noreply.github.com>
2022-11-10 16:30:35 +0900

    백준 4673번

---
+++ c#/bj4673.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+namespace test2
+{
+    internal class Program
+    {
+        static int d(int i)
+        {
+            int total = i;
+            while (i != 0)
+            {
+                total += (i % 10);
+                i /= 10;
+            }
+            return total;
+        }
+        static void Main(string[] args)
+        {
+            bool[] ans = new bool[10001];
+            for (int i = 1; i < 10001; i++)
+            {
+                int a = d(i);
+                if (a < 10001)
+                {
+                    ans[a] = true;
+                }
+            }
+            for (int i = 1; i < 10001; i++)
+            {
+                if (!ans[i])
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+
+}
