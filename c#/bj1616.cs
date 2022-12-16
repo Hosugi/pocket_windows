5999cc2e7b6314d63ad21cba1bb824329536d9fc
Hosugi <108512016+Hosugi@users.noreply.github.com>
2022-12-16 11:36:58 +0900

    백준 1616번
    답 찾아보고 c# 버전으로 바꿈 (이걸 어떻게 알아내누...)

---
+++ c#/bj1616.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test2
+{
+    internal class bj1616
+    {
+
+        
+        static void Main(string[] args)
+        {
+            int n  = int.Parse(Console.ReadLine());
+            int count = 0;
+            if(n == 0) { Console.WriteLine(count); return; }            //팩토리얼에 0x0은 0
+
+            for (int i = 2; i <= n; i++)
+            {
+                if (i % 5 == 0)                                         // 5의 배수일 때 0이 하나씩 늘어남
+                {
+                    count++;
+                }
+                if (i % 25 == 0)                                        // 25의 배수일 때 0이 하나씩 늘어남
+                {
+                    count++;
+                }
+                if (i % 125 == 0)                                       // 125 배수일때.....
+                {
+                    count++;
+                }
+            }
+            // 솔직히 답 찾아봤음..
+            Console.WriteLine(count);
+            Console.ReadKey();
+        }
+    }
+}
